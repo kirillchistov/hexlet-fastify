1b42ba8ce7e5624d0a8a3343212f560227dfd230
Kirill Chistov <kirill.v.chistov@yandex.ru>
2026-07-18 11:30:22 +0300

    Dynamic routes commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import fastify from 'fastify'
 
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user',
+    },
+  ],
+}
+
 export default () => {
   const app = fastify()
 
@@ -14,6 +23,26 @@ export default () => {
   app.get('/hello', (req, res) => {
     const name = req.query.name || 'World'
     res.send(`Hello, ${name}!`)
+  })
+
+  app.get('/users/new', (req, res) => {
+    res.send('User build')
+  })
+
+  app.get('/users/:id', (req, res) => {
+    const { id } = req.params
+    const user = state.users.find((user) => user.id === parseInt(id, 10))
+
+    if (!user) {
+      res.code(404).send({ message: 'User not found' })
+      return
+    }
+
+    res.send(user)
+  })
+
+  app.get('/users/:id/post/:postId', (req, res) => {
+    res.send(`User ID: ${req.params.id}; Post ID: ${req.params.postId}`)
   })
 
   return app
