ad67f7cd9ac285d57684b03b9da2de7fb9cc0c83
Kirill Chistov <kirill.v.chistov@yandex.ru>
2026-07-18 12:35:50 +0300

    Search lesson

--- src/index.js
+++ src/index.js
@@ -35,13 +35,103 @@ const state = {
   courses: [
     {
       id: 1,
-      title: 'JS: Массивы',
-      description: 'Курс про массивы в JavaScript',
+      title: 'Python-разработчик',
+      description: 'Изучите Python, Django, REST и Fast API для создания веб-приложений',
     },
     {
       id: 2,
-      title: 'JS: Функции',
-      description: 'Курс про функции в JavaScript',
+      title: 'Фронтенд-разработчик',
+      description: 'Изучите HTML, CSS, JavaScript и React',
+    },
+    {
+      id: 3,
+      title: 'Java-разработчик',
+      description: 'Изучите Java и фреймворк Spring Boot и REST API',
+    },
+    {
+      id: 4,
+      title: 'PHP-разработчик',
+      description: 'Изучите PHP и Laravel для разработки и проектирования REST API',
+    },
+    {
+      id: 5,
+      title: 'Go-разработчик',
+      description: 'Изучите Go, работу с БД, HTTP, конкурентность, горутины, многопоточность',
+    },
+    {
+      id: 6,
+      title: 'Node.js-разработчик',
+      description: 'Изучите JavaScript, Node.js, Fastify и REST API',
+    },
+    {
+      id: 7,
+      title: 'Fullstack-разработчик на Node.js',
+      description: 'Освоите JavaScript, Node.js, Fastify и React для фронтенда и бэкенда',
+    },
+    {
+      id: 8,
+      title: 'Веб-разработка на Fastify',
+      description: 'Получите навык создания бэкенда и построения HTTP API',
+    },
+    {
+      id: 9,
+      title: 'Основы Python',
+      description: 'Изучите синтаксис, переменные, функции',
+    },
+    {
+      id: 10,
+      title: 'Основы JavaScript',
+      description: 'Изучите язык JavaScript и его практическое применение',
+    },
+    {
+      id: 11,
+      title: 'React',
+      description: 'Освоите React и создание быстрых интерфейсов',
+    },
+    {
+      id: 12,
+      title: 'Django',
+      description: 'Изучите фреймворк Django для создания веб-приложений',
+    },
+    {
+      id: 13,
+      title: 'Laravel',
+      description: 'Изучите фреймворк Laravel для создания веб-приложений',
+    },
+    {
+      id: 14,
+      title: 'Spring Boot',
+      description: 'Навык работы с Spring Boot для масштабируемых веб-приложений',
+    },
+    {
+      id: 15,
+      title: 'REST API в Node.js',
+      description: 'Навык разработки высокопроизводительных API',
+    },
+    {
+      id: 16,
+      title: 'Typescript',
+      description: 'Изучите Typescript и получите навык снижать ошибки, упрощать отладку',
+    },
+    {
+      id: 17,
+      title: 'Основы Go',
+      description: 'Изучите основы Go, структуры, функции и горутины',
+    },
+    {
+      id: 18,
+      title: 'Веб-разработка на Express',
+      description: 'Изучение микрофреймворка Express для создания веб-приложений на JavaScript',
+    },
+    {
+      id: 19,
+      title: 'Основы Java',
+      description: 'Изучите базовые концепции: переменные, условия и циклы',
+    },
+    {
+      id: 20,
+      title: 'Основы PHP',
+      description: 'Изучите основы PHP и создание простых веб-страниц',
     },
   ],
 }
@@ -102,9 +192,30 @@ export default async () => {
   })
 
   app.get('/courses', (req, reply) => {
+    const term = req.query.term ?? null
+    const descriptionTerm = req.query.descriptionTerm ?? null
+
+    let courses = state.courses
+
+    if (term !== null && term !== '') {
+      const normalizedTerm = term.toLowerCase()
+      courses = courses.filter(({ title }) =>
+        title.toLowerCase().includes(normalizedTerm),
+      )
+    }
+
+    if (descriptionTerm !== null && descriptionTerm !== '') {
+      const normalizedDescriptionTerm = descriptionTerm.toLowerCase()
+      courses = courses.filter(({ description }) =>
+        description.toLowerCase().includes(normalizedDescriptionTerm),
+      )
+    }
+
     reply.view('courses/index', {
       title: 'Курсы',
-      courses: state.courses,
+      term: term ?? '',
+      descriptionTerm: descriptionTerm ?? '',
+      courses,
       header: 'Курсы по программированию',
     })
   })
